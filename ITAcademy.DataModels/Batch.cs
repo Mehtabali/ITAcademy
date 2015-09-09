e3fc2278208bc952255e694729046c861b4fc869
iiqraali <iiqraali@hotmail.com>
2015-09-09 23:03:58 +0500

    I have updated winforms

---
+++ ITAcademy.DataModels/Batch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITAcademy.DataModels
+{
+    class Batch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan Timings { get; set; }
+        public int Courses_Id { get; set; }
+        public int Users_Id { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
---
+++ ITAcademy.DataModels/Users_Type.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITAcademy.DataModels
+{
+    class Users_Type
+    {
+
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+    
+    }
+}
